9eeb9a0f77074a8ccc572ee79613df025c3284ae
Lilly Liefers <lillyliefers@me.com>
2023-03-30 17:51:36 +0200

    Solved Lab

--- src/main.js
+++ src/main.js
@@ -4,7 +4,30 @@ function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
 }
+function preload(){
+  game.preload()
+}
 
 function draw() {
+  clear()
   game.drawGrid();
+  game.player.draw()
 }
+
+function keyPressed() {
+    if (keyCode === 37) {
+      game.player.moveLeft()
+    }
+  
+    if (keyCode === 39) {
+       game.player.moveRight()
+     }
+  
+     if (keyCode === 38) {
+       game.player.moveUp()
+     }
+  
+     if (keyCode === 40) {
+       game.player.moveDown()
+     }
+   }
