2c6122531a5a9e3a59fd7ffd6cda4ba5a626d6a3
Alexandra Strang <bouadloun1@gmail.com>
2023-03-30 17:51:45 +0200

    treasure hunt

--- src/game.js
+++ src/game.js
@@ -1,7 +1,45 @@
 class Game {
+  constructor() {
+    this.player = new Player();
+  }
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    // line(10, 100, width, 100);
+
+    for (let i = 0; i <= width; i += 100) {
+      line(0, i, width, i);
+      line(i, 0, i, height);
+    }
+  }
+  preload() {
+    this.player.image = loadImage("../assets/character-down.png");
   }
 }
+
+class Player {
+  constructor() {
+    this.image;
+    this.col = 0;
+    this.row = 0;
+  }
+  moveUp() {
+    this.col -= 100;
+  }
+  moveDown() {
+    this.col += 100;
+  }
+  moveLeft() {
+    this.row -= 100;
+  }
+  moveRight() {
+    this.row += 100;
+  }
+  draw() {
+    image(this.image, this.row, this.col, 100, 100);
+  }
+}
+
+// const player = new Player(0, 0); // (0,0) = Initial position (col, row)
+// player.moveDown(); // Increase by 1 the value of player.row
+// player.moveDown(); // Increase by 1 the value of player.row
+// player.moveRight(); // Increase by 1 the value of player.col
+// console.log(player.col, player.row); // => 1,2
