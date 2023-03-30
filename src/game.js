8effb10510a37a932804b09ab115f3747b456233
Matt N <mneckelmann@gmail.com>
2023-03-30 17:34:43 +0200

    Solved

--- src/game.js
+++ src/game.js
@@ -1,7 +1,67 @@
 class Game {
+  constructor() {
+    this.player = new Player();
+    this.treasure = new Treasure();
+  }
+
+  preload() {
+    this.player.image = loadImage("../assets/character-down.png");
+    this.treasure.image = loadImage("../assets/treasure.png");
+    this.treasure.setRandomPosition();
+  }
+
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    clear();
+    for (let i = 0; i <= 1000; i += 100) {
+      strokeWeight(2);
+      line(i, 0, i, 1000);
+      line(0, i, 1000, i);
+    }
+    this.player.draw();
+  }
+}
+
+class Player {
+  constructor() {
+    this.col = 0;
+    this.row = 0;
+    this.width = 100;
+    this.height = 100;
+    this.image;
+  }
+  moveLeft() {
+    this.col--;
+  }
+  moveRight() {
+    this.col++;
+  }
+  moveUp() {
+    this.row--;
+  }
+  moveDown() {
+    this.row++;
+  }
+
+  draw() {
+    image(this.image, this.col * 100, this.row * 100, this.width, this.height);
+  }
+}
+
+class Treasure {
+  constructor() {
+    this.col = 1;
+    this.row = 1;
+    this.width = 100;
+    this.height = 100;
+    this.image;
+  }
+
+  setRandomPosition() {
+    this.col = Math.floor(Math.random() * 9) + 1;
+    this.row = Math.floor(Math.random() * 9) + 1;
+  }
+
+  drawTreasure() {
+    image(this.image, this.col * 100, this.row * 100, this.width, this.height);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 const game = new Game();
 
+// const player = new Player();
+
+function preload() {
+  game.preload();
+}
+
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
@@ -7,4 +13,23 @@ function setup() {
 
 function draw() {
   game.drawGrid();
+  game.treasure.drawTreasure();
+}
+
+function keyPressed() {
+  if (keyCode === 37) {
+    game.player.moveLeft();
+  }
+
+  if (keyCode === 39) {
+    game.player.moveRight();
+  }
+
+  if (keyCode === 38) {
+    game.player.moveUp();
+  }
+
+  if (keyCode === 40) {
+    game.player.moveDown();
+  }
 }
