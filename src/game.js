6c3c49f48d44b3901a056f4833ac48ec9e49c18f
bojana-petroska <bojana.petroska@yahoo.com>
2023-03-30 17:55:23 +0200

    treasure

--- src/game.js
+++ src/game.js
@@ -3,5 +3,25 @@ class Game {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+    createCanvas(500, 500)
+    background(200)
+
+    let rows = 10
+    let columns = 10
+
+    let itemWidth = width / columns
+    let itemHeight = height / rows
+
+    for (let i=0; i<rows; i++) {
+      for (let j=0; j<columns; j++) {
+        let x = j * itemWidth;
+        let y = i * itemHeight
+
+        if ((i + j) % 2 === 0) {
+          fill(255)
+          rect(x, y, itemWidth, itemHeight)
+        }
+      }
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,109 @@
-const game = new Game();
+
+
+class Player {
+  constructor(col, row) {
+    this.col = col
+    this.row = row
+    this.image
+  }
+  
+  moveUp() {
+    this.col -= 50
+  }
+  
+  moveDown() {
+    this.col += 50
+  }
+  
+  moveLeft() {
+    this.row -= 50
+  }
+  
+  moveRight() {
+    this.row += 50
+  }
+  
+  draw() {
+    image(this.image, this.row, this.col, 50, 50)
+  }
+  
+  preload() {
+    this.image = loadImage("../assets/character-down.png")
+  }
+  
+}
+
+
+
+function preload() {
+  player.preload()
+  treasure.preload()
+}
 
 function setup() {
-  let canvas = createCanvas(WIDTH, HEIGHT);
+  let canvas = createCanvas();
   canvas.parent("canvas");
 }
 
 function draw() {
   game.drawGrid();
+  player.draw();
+  treasure.draw()
 }
+
+function keyPressed() {
+  //console.log('key')
+  if (keyCode === 39) {
+    player.moveRight()
+	}
+  
+	if (keyCode === 37) {
+    player.moveLeft()
+	}
+  
+	if (keyCode === 38) {
+    player.moveUp()
+	}
+  
+	if (keyCode === 40) {
+    player.moveDown()
+	}
+  if (player.row === treasure.row && player.col === treasure.col) {
+    treasure.found()
+  }
+  
+  clear()
+}
+
+
+class Treasure {
+  constructor() {
+    this.col
+    this.row
+    this.image
+  }
+  setRandomPosition() {
+    this.col = Math.floor(Math.random() * 10)
+    this.row = Math.floor(Math.random() * 10)
+  }
+  
+  draw() {
+    image(this.image, this.row, this.col, 50, 50)
+  }
+  
+  preload() {
+    this.image = loadImage("../assets/treasure.png")
+  }
+  
+  found() {
+    this.setRandomPosition()
+  }
+}
+
+const game = new Game();
+let treasure = new Treasure(50, 50);
+let player = new Player(50, 50);
+treasure.setRandomPosition();
+
+
+
