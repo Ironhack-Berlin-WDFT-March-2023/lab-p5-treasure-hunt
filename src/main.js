196b2bba33ba95f239d3f98f17afaaf84ef96f8d
Work <work@Bartholomaus.local>
2023-03-30 17:25:53 +0200

    Done

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 const game = new Game();
 
+function preload() {
+  game.preload()
+}
+
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
@@ -7,4 +11,21 @@ function setup() {
 
 function draw() {
   game.drawGrid();
+  game.player.draw();
+  game.treasure.drawTreasure();
 }
+
+function keyPressed() {
+  if(keyCode === LEFT_ARROW) {
+    if(game.player.col > 0) game.player.moveLeft()
+  }
+  else if(keyCode === RIGHT_ARROW) {
+    if(game.player.col < 9) game.player.moveRight()
+  }
+  else if(keyCode === UP_ARROW) {
+    if(game.player.row > 0) game.player.moveUp()
+  }
+  else if(keyCode === DOWN_ARROW) {
+    if(game.player.row < 9) game.player.moveDown()
+}
+  }
