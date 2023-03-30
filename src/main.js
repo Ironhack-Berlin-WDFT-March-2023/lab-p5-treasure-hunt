c0cbc78c50e516d04a43ad5bd7c5b5a90a5ebf89
Nadezda Samorokova <nadezdasamorokova@gmail.com>
2023-03-30 17:50:43 +0200

    Steps 1 to 4

--- src/main.js
+++ src/main.js
@@ -5,6 +5,30 @@ function setup() {
   canvas.parent("canvas");
 }
 
+function preload() {
+  game.preload();
+}
+
 function draw() {
+  clear()
   game.drawGrid();
+  game.player.draw();
+}
+
+function keyPressed() {
+	if (keyCode === 39) {
+		game.player.moveRight()
+	}
+
+	if (keyCode === 37) {
+		game.player.moveLeft()
+	}
+
+	if (keyCode === 38) {
+		game.player.moveUp()
+	}
+
+	if (keyCode === 40) {
+		game.player.moveDown()
+	}
 }
