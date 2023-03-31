80659047424c03d6de085a73ba2f3b8b77d0771a
hussain <muhammadhussain170117@gmail.com>
2023-03-31 10:25:47 +0200

    initial commit

--- src/game.js
+++ src/game.js
@@ -1,7 +1,65 @@
 class Game {
-  drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+  constructor() {
+    this.player = new Player();
   }
+  
+  preload(){
+    this.player.image=loadImage("../assets/character-down.png")
+    this.player.imageUp=loadImage("../assets/character-up.png")
+    this.player.imageDown=loadImage("../assets/character-down.png")
+    this.player.imageLeft=loadImage("../assets/character-left.png")
+    this.player.imageRight=loadImage("../assets/character-right.png")
+
+  }
+  draw() {
+    clear();
+    this.drawGrid()
+    this.player.draw()
+    
+  }
+  drawGrid() {
+    background(220);
+    for (var x = 0; x < width + 1; x += width / 10) {
+      for (var y = 0; y < height + 1; y += height / 10) {
+        stroke(0);
+        strokeWeight(1);
+        line(x, 0, x, height);
+        line(0, y, width, y);
+      }
+    }
+  }
+}
+
+class Player {
+  constructor() {
+    this.col = 0;
+    this.row = 0;
+    this.imageUp
+    this.imageDown
+    this.imageLeft
+    this.imageRight
+    this.image
+ 
+  }
+  draw(){
+    image(this.image,this.col,this.row,100,100)
+  }
+
+  moveUp() {
+    this.row -= 1;
+  }
+  moveDown() {
+    this.row += 1;
+  }
+  moveLeft() {
+    this.col -= 1;
+  }
+  moveRight() {
+    this.col += 1;
+  }
+ 
+}
+
+function hello(){
+  
 }
