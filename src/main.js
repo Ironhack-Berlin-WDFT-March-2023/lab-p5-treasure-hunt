5db7f38753ecc73c6585cfde3bdb5eaa35e613ca
Silvia Flare <silviaguizan@gmail.com>
2023-03-31 09:13:15 +0200

    Solved 1-4 iteraction, 5 it´s on work

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 const game = new Game();
+const player = new Player();
+const treasure = new Treasure();
+
+let charDown, charUp, charLeft, charRight, treasureImg;
+
+function preload() {
+  console.log("preload");
+
+  charDown = loadImage("../assets/character-down.png");
+  charUp = loadImage("../assets/character-up.png");
+  charLeft = loadImage("../assets/character-left.png");
+  charRight = loadImage("../assets/character-right.png");
+  treasureImg = loadImage("../assets/treasure.png");
+}
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
@@ -6,5 +20,31 @@ function setup() {
 }
 
 function draw() {
+  clear();
   game.drawGrid();
+  player.draw();
+  treasure.draw();
+}
+
+function keyPressed() {
+  if (keyCode === ENTER) {
+    console.log("enter");
+    treasure.setRandomPosition();
+  }
+  if (keyCode === LEFT_ARROW) {
+    console.log("left");
+    player.moveLeft();
+  }
+  if (keyCode === RIGHT_ARROW) {
+    console.log("right");
+    player.moveRight();
+  }
+  if (keyCode === UP_ARROW) {
+    console.log("left");
+    player.moveUp();
+  }
+  if (keyCode === DOWN_ARROW) {
+    console.log("right");
+    player.moveDown();
+  }
 }
